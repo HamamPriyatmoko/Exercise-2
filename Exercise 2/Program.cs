79f4621a888e2c01067d8c69a5435dcf42b1c458
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-03 16:59:04 +0700

    membuat function static

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -51,67 +51,63 @@ namespace exercise2
         }
         public void q_sort(int low, int high)
         {
-            int pivot, i, j;
-            if (low > high)
-                return;
-
-            //Partition the list into two parts:
-            // one containing elements less that or equal to pivot
-            // outher containning elements greather than pivot
-            i = low + 1;
-            j = high;
-
-            pivot = arr[low];
-
-            while (i <= j)
+            char ch;
+            do
             {
-                //Search for an element greater than pivot
-                while ((arr[i] <= pivot) && (i <= high))
-                {
-                    i++;
-                    cmp_count++;
-                }
-                cmp_count++;
+                int pivot, i, hp;
+                if (low > high)
+                    return;
 
-                //Search for an element lens than or equal to pivot
-                while ((arr[j] > pivot) && (i >= low))
-                {
-                    j--;
-                    cmp_count++;
-                }
-                cmp_count++;
+                //Partition the list into two parts:
+                // one containing elements less that or equal to pivot
+                // outher containning elements greather than pivot
+                i = low + 1;
+                hp = high;
 
-                //Search forn an element less than or equal to pivot
-                if (i < j) // if the greater element is on the left of the element
+                pivot = arr[low];
+
+                while (i <= hp)
                 {
-                    //Swap the element at index i with the element at index j
-                    swap(i, j);
+                    //Search for an element greater than pivot
+                    while ((arr[i] <= pivot) && (i <= high))
+                    {
+                        i++;
+                        cmp_count++;
+                    }
+                    cmp_count++;
+
+                    //Search for an element lens than or equal to pivot
+                    while ((arr[hp] > pivot) && (i >= low))
+                    {
+                        hp--;
+                        cmp_count++;
+                    }
+                    cmp_count++;
+
+                    //Search forn an element less than or equal to pivot
+                    if (i < hp) // if the greater element is on the left of the element
+                    {
+                        //Swap the element at index i with the element at index j
+                        swap(i, j);
+                        mov_count++;
+                    }
+                }
+                if (low < j)
+                {
+                    swap(low, j);
                     mov_count++;
                 }
-            }
-            if (low < j)
-            {
-                swap(low, j);
-                mov_count++;
-            }
-            // sort the list on the left of the pivot using quick sort
-            q_sort(low, j - 1);
+                // sort the list on the left of the pivot using quick sort
+                q_sort(low, j - 1);
 
-            //Sort the list on the right of pivot using quick sort
-            q_sort(j + 1, high);
+                //Sort the list on the right of pivot using quick sort
+                q_sort(j + 1, high);
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
         }
-        void display()
+        int getSize()
         {
-            Console.WriteLine("\n----------------------");
-            Console.WriteLine(" Sort array elements");
-            Console.WriteLine("------------------------");
-
-            for (int j = 0; j < n; j++)
-            {
-                Console.WriteLine(arr[j]);
-            }
-            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
-            Console.WriteLine("\nNumber of data movements : " + mov_count);
+            return n;
         }
 
         void BinarySearch()
@@ -151,7 +147,53 @@ namespace exercise2
                 ch = char.Parse(Console.ReadLine().ToUpper());
             } while ((ch == 'y'));
         }
-
+        static void Main(string[] args)
+        {
+            Program mylist = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("==================");
+                    Console.WriteLine("1. binary search");
+                    Console.WriteLine("2. q_sort");
+                    Console.WriteLine("3. exit");
+                    Console.WriteLine("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("....................");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine("....................");
+                            mylist.input();
+                            mylist.BinarySearch();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("....................");
+                            Console.WriteLine("q_sort ");
+                            Console.WriteLine("....................");
+                            mylist.input();
+                            mylist.q_sort();
+                            break;
+                        case 3:
+                            Console.WriteLine("Exit");
+                            break;
+                    }
+                    Console.WriteLine("\nPilih menu lagi? (y/n): ");
+                    ch = char.Parse(Console.ReadLine().ToLower());
+                    Console.Clear();
+                } while (ch == 'y');
+                //to exit from the console
+                Console.WriteLine("\n\nPress return to exit. ");
+                Console.ReadLine();
+            } while (pilihanmenu != 3);
+        }
     }
 
 }
