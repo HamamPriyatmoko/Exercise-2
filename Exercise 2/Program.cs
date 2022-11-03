ba83f045bfbdf867c188d205d06787c2ba3c1b80
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-03 16:40:19 +0700

    membuat function BinarySearch dan hapus function data

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -113,33 +113,45 @@ namespace exercise2
             Console.WriteLine("\nNumber of comparisons: " + cmp_count);
             Console.WriteLine("\nNumber of data movements : " + mov_count);
         }
-        void data()
-        {
-            while (true)
-            {
 
-                Console.Write("enter the number of element in the array :");
-                string s = Console.ReadLine();
-                n = Int32.Parse(s);
-                if ((n > 0) && (n <= 20))
-                    break;
-                else
-                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
-            }
-            //accept array element
-            Console.WriteLine("");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine(" Enter Array Element ");
-            Console.WriteLine("-----------------------");
-            for (i = 0; i < n; i++)
+        void BinarySearch()
+        {
+            char ch;
+            do
             {
-                Console.WriteLine("<" + (i + 1) + ">");
-                string s1 = Console.ReadLine();
-                arr[i] = int.Parse(s1);
-            }
+                //accept the number to be searched
+                Console.Write("\nEnter the element you want to search : ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary search
+                int lowerbound = 0;
+                int upperbound = 0;
+
+                //obtain the index of the middle element
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the element in the array 
+                while ((item != arr[mid]) && (ctr <= upperbound))
+                {
+                    if (item == arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " Not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinur search (y/n): ");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
         }
-       
-        
+
     }
 
 }
